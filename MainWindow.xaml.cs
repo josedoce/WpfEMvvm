9fab9e3391a9c4d17bde6a20e545238fd4f419d3
josedoce <josesamya@gmail.com>
2023-02-21 15:23:40 -0300

    Adicionar arquivos de projeto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEMvvm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Veiculo veiculo;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            veiculo = new Veiculo();
+            veiculo.VeiculoName = "Constellation";
+            //DataContext = veiculo;
+            VeiculoViewModel veiculo2 = new VeiculoViewModel();
+            veiculo2.VeiculoNome = "Vectra Turbo";
+            DataContext = veiculo2;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            veiculo.VeiculoName = "Prisma tsi";
+            MessageBox.Show(string.Format("O veiculo agora é {0}.", veiculo.VeiculoName));
+        }
+    }
+
+    public class Veiculo
+    {
+        private string veiculo;
+        public string VeiculoName
+        {
+            get { return veiculo; }
+            set { veiculo = value; }
+        }
+    }
+}
---
+++ VeiculoViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfEMvvm
+{
+    public class VeiculoViewModel : INotifyPropertyChanged
+    {
+        private string veiculo;
+
+       
+
+        public ICommand MudarCommand { get; set; }
+
+
+        //sem essa propriedade, não poderemos alterar nada.
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public VeiculoViewModel() {
+            MudarCommand = new RelayCommand(AlgumaLogica, CanExecutarLogica);
+        }   
+
+        public bool CanExecutarLogica()
+        {
+            //execute algo se tiver validado.
+            return true;
+        }
+
+        public void AlgumaLogica()
+        {
+            VeiculoNome = "Monza Biturbo";
+            MessageBox.Show("Logica...");
+        }
+
+        public string VeiculoNome
+        {
+            get { return veiculo; }
+            set
+            {
+                veiculo = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
